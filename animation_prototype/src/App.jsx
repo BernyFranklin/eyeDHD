b0befd03cb109190215edf963111d6a83a5867a4
MiguelIbarra2021 <miguel.ibarra2021@gmail.com>
2025-10-16 16:30:38 -0700

    Add base prototype for webgl animation

---
+++ animation_prototype/src/App.jsx
@@ -0,0 +1,13 @@
+import EyeScene from './components/EyeScene.jsx'
+
+function App() {
+  return (
+    <>
+      <div className='animation'>
+        <EyeScene />
+      </div>
+    </>
+  )
+}
+
+export default App
