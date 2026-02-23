fe8f62ab8667e1a99d5b36da07542c5e5beea624
dwclake <clake.dw@gmail.com>
2026-02-23 13:00:56 -0800

    fixed import error in src/types.ts, move error type to Error.ts

--- EyeDHD-App/src/types.ts
+++ EyeDHD-App/src/types.ts
@@ -1,15 +1,12 @@
+/**
+ * Re-exports types from the Electron main process to be accessed easier in the React
+ * renderer code.
+ */
+
 export { type CSVData } from '../electron/db/tables/csv';
 export { type Metadata } from '../electron/db/tables/metadata';
 export { type SaccadeData } from '../electron/db/tables/saccade';
 
-export { type DataType, type StreamKey, type StreamType, type Progress } from "../electron/db/DataStream";
+export { type DataType, type StreamKey, type StreamType, type Progress } from '../electron/db/DataStream';
 
-export type Error = UIError | ElectronError;
-
-interface UIError {
-	message: string
-}
-
-interface ElectronError {
-	message: string
-}
+export { type Error } from './Error';
