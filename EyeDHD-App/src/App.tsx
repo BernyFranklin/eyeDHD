415dd17a3166c8cdce73b4e980d512216aab6087
dwclake <clake.dw@gmail.com>
2026-02-25 14:32:48 -0800

    converted to react-router, replacing content components array

--- EyeDHD-App/src/App.tsx
+++ EyeDHD-App/src/App.tsx
@@ -1,15 +1,8 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React from 'react';
 
 import './App.css';
-import HomePage from './components/HomePage';
-import CsvFileImport from './components/CsvFileImport';
-import AnimationGenerator from './components/AnimationGenerator';
-import { Visualization } from './components/visualization';
-import LoadingOverlay from './components/LoadingOverlay';
 import Navbar from './components/Navbar';
-import SidebySide from './components/SidebySide';
-import CaseList from './components/CaseList';
-import { BrowserRouter, Outlet } from 'react-router';
+import { Outlet } from 'react-router';
 
 type Props = {
 	buttonsDisabled: boolean
