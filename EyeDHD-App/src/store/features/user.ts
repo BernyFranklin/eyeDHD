8266e11fc82b8846a72ca44667f07d140375eac3
dwclake <clake.dw@gmail.com>
2026-02-26 12:05:08 -0800

    basic redux storage for button enable / disable

--- EyeDHD-App/src/store/features/user.ts
+++ EyeDHD-App/src/store/features/user.ts
@@ -1,11 +1,12 @@
 import { type Metadata } from '../../types';
 
 interface Options {
-
+	stuff: void;
 }
 
 interface User {
 	dir: string;
 	options: Options;
+	cases: Metadata[];
 	selectedCase?: Metadata;
 }
