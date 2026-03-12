e6a67992e315130314b46d4e3aa7cd2b8d013d14
Frank Bernal <frankbernal@mail.fresnostate.edu>
2026-03-12 09:35:22 -0700

    Added tests, types, and implementation files for the visualization prep. Moving to branch, will merge every time a section is completed. I.E Section A, B, C...

---
+++ EyeDHD-App/saccades/visualization/prep/prepareVisualizationData.ts
@@ -0,0 +1,2 @@
+import { VisualizationPrepInput, VisualizationPrepOptions } from "./types";
+export function prepareVisualizationModels(input: VisualizationPrepInput, options?: VisualizationPrepOptions) {}
